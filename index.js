e4bc3a8355b7dd69e627bd02af45c888141a0302
Jonathan Henriksen <jonathan.henriksen@yandex.com>
2018-04-16 22:02:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+var express = require('express')
+var app = express()
+var ambilux = require("ambilux")
+
+// LIFX Configuration
+var LifxClient = require('node-lifx').Client;
+var client = new LifxClient();
+var bulb
+client.on('light-new', function (light) {
+  bulb = light;
+});
+client.init();
+
+const colorList = {
+  "red": [0, 0, 2],
+  "green": [0, 0, 0],
+  "blue": [0, 0, 0],
+  "yellow": [0, 0, 0],
+  "": [0, 0, 0]
+}
+
+app.get('/lamp-toggle', function (req, res) {
+  bulb.getPower((error, power) => {
+    if (power == 1) {
+      bulb.off(1500)
+      res.send('lamp-off')
+    } else {
+      bulb.on(2000)
+      res.send('lamp-on')
+    }
+  })
+})
+
+app.get('/fade-up', function (req, res) {
+  fade((color) => {
+    if (color.brightness > 95) {
+      bulb.color(color.hue, color.saturation, 100)
+    } else {
+      bulb.color(color.hue, color.saturation, color.brightness + 5)
+    }
+    res.send('fade-up')
+  })
+})
+
+app.get('/fade-down', function (req, res) {
+  fade((color) => {
+    if (color.brightness < 5) {
+      bulb.color(color.hue, color.saturation, 0)
+    } else {
+      bulb.color(color.hue, color.saturation, color.brightness - 5)
+    }
+    res.send('fade-down')
+  })
+})
+
+
+// Helper
+var fade = function (cb) {
+  bulb.getPower((error, power) => {
+    if (!error) {
+      if (power == 1) {
+        bulb.getState((error, data) => {
+          return cb(data.color);
+        })
+      }
+    } else {
+      return;
+    }
+  })
+
+  var changeColor = function (color) {
+    bulb.getPower((error, data) => {
+      if (power == 1) {
+        bulb.colorRgb(color.red, color.green, color.blue)
+        res.send('changed light color')
+      }
+    })
+  }
+}
+
+app.listen(80)
